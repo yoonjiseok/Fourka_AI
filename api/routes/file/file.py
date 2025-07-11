4f4febc5acba7cc8b5b0b1b3fbb620ff946a8837
KwonHalim <hr_blizzard@naver.com>
2025-07-11 09:42:12 +0900

    :tada: 프로젝트 전체 구조 생성

--- api/routes/file/file.py
+++ api/routes/file/file.py
@@ -1,3 +1,20 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Security, UploadFile, File
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+import service.file.file_service as file_service
+from api.routes.chat.chat import chat_router
 
 file_router = APIRouter(prefix="/api/files", tags=["files"])
+
+
+security_scheme = HTTPBearer()
+
+
+@chat_router.post("/pdf/upload")
+async def upload_pdf(
+        token: Annotated[..., HTTPAuthorizationCredentials, Security(security_scheme)],
+        file: Annotated[UploadFile, File(..., description="업로드할 PDF 파일")]
+):
+    result = file_service.read_file(file)
---
+++ service/file/file_service.py
@@ -0,0 +1,6 @@
+from fastapi import File
+import fitz
+
+def read_file(file: File):
+    content = fitz.open(file)
+    # 임베딩 시작
