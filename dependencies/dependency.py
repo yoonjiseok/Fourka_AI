86be3ea05e80d3cae47797d1cdfc6acc7319c261
KwonHalim <hr_blizzard@naver.com>
2025-07-14 10:40:08 +0900

    :sparkles: [feat] 파일 구조 리팩토링
    의존성 주입 함수 따로 만듦

---
+++ dependencies/dependency.py
@@ -0,0 +1,29 @@
+# 의존성 주입 파일
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from utils.db import get_db
+from database.repository.file_repository import FileRepository
+from service.file.file_service import FileService
+
+def get_file_repository(db: AsyncSession = Depends(get_db)) -> FileRepository:
+    return FileRepository(db)
+
+def get_file_service(repo: FileRepository = Depends(get_file_repository)) -> FileService:
+    return FileService(repo)
+
+
+"""
+# 만약 chat_repository와 서비스를 따로 만든다면
+def get_chat_repository(db: AsyncSession = Depends(get_db)) -> chatRepository:
+    return chatRepository(db)
+
+def get_chat_service(repo: chatRepository = Depends(get_chat_repository)) -> chatService:
+    return chatService(repo)
+    
+작성후 
+
+컨트롤러의 Depends에 해당 함수들을 추가로 작성하면 됨
+만약 서비스 계층에서 두개의 레포지토리를 사용해야한다면
+__init__에서 Repository를 주입받으면 됨
+"""
