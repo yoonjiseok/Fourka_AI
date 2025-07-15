93ac53afc607e458247284abec5964d616d0a630
G9bonwook <gubon0830@gmail.com>
2025-07-15 14:46:47 +0900

    ✨ [feature] #13 pdf 업로드 라우터 경로 생성

---
+++ api/routes/document/document.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter, Security, UploadFile, File, Depends, Form
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from typing import Annotated
+
+from dependencies.dependency import get_document_service
+from api.routes.document import documentDTO
+from service.document.document_service import DocumentService
+
+document_router = APIRouter(prefix="/api/documents", tags=["files"])
+
+
+security_scheme = HTTPBearer()
+
+@document_router.patch("/pdf/update")
+async def update_pdf(
+        token: Annotated[HTTPAuthorizationCredentials, Security(security_scheme)],
+        fileinfo: documentDTO.UpdateDTO,
+        document_service: DocumentService = Depends(get_document_service)
+):
+    await document_service.update_file_name(
+        file_id=fileinfo.doc_id,
+        title=fileinfo.title
+    )
+
+    return {"message": "File information updated successfully"}
+
+
+@document_router.post("/pdf/upload")
+async def upload_pdf(
+    title: str = Form(...),
+    version: str = Form(...),
+    folder_id: int = Form(...),
+    commit_message: str = Form(...),
+    file: UploadFile = File(...),
+    document_service: DocumentService = Depends(get_document_service)
+):
+    # Form 데이터를 DTO로 변환
+    fileinfo = documentDTO.UploadDTO(
+        title=title,
+        version=version,
+        folder_id=folder_id,
+        commit_message=commit_message
+    )
+    
+    await document_service.upload_pdf(
+        file=file,
+        title=fileinfo.title,
+        version=fileinfo.version,
+        folder_id=fileinfo.folder_id,
+        commit_message=fileinfo.commit_message
+    )
+
+    return {"message": "File uploaded successfully"}
