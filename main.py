09db668b5d9cb9f023955f0988c5d32d0cf202eb
G9bonwook <gubon0830@gmail.com>
2025-07-15 14:57:48 +0900

    ✨ [feature] #13 서버 실행시 DB 자동 생성하도록 startup_event 함수 추가

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 from api.routes.chat.chat import chat_router
-from api.routes.file.file import file_router
+from api.routes.document.document import document_router
+from database import models
+from utils.db import async_engine
+import asyncio
 
 app = FastAPI(
     title="CHATBOT",
     description="Rag AI Chatbot ",
 )
 
+@app.on_event("startup")
+async def startup_event():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(models.Base.metadata.create_all)
 
 app.include_router(chat_router)
-app.include_router(file_router)
+app.include_router(document_router)
