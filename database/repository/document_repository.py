e8d5ca3e02c9d45b519bd570ce521e01a1456e02
G9bonwook <gubon0830@gmail.com>
2025-07-15 16:12:24 +0900

    🔨 [fix] #13 repository 반환값 수정

--- database/repository/document_repository.py
+++ database/repository/document_repository.py
@@ -8,22 +8,23 @@ class DocumentRepository:
 
     async def update_file_name(self, doc_id: int, title: str) -> None:
         """
-        비동기 방식으로 문서의 제목을 업데이트하고 변경사항을 커밋합니다.
+        비동기 방식으로 문서의 제목을 업데이트합니다.
         """
-        
         # 문서 조회
         document = await self.db.get(Document, doc_id)
         if document:
             # 제목 업데이트
             document.title = title
             await self.db.commit()
+            await self.db.refresh(document)
+            return document.doc_id, document.title
 
     async def create_document(self, title: str, version: str, folder_id: int, commit_message: str) -> None:
         """
         새로운 문서를 데이터베이스에 저장합니다.
         """
         from database.models import Document
-        
+     
         new_document = Document(
             title=title,
             version=version,
@@ -34,4 +35,4 @@ class DocumentRepository:
         self.db.add(new_document)
         await self.db.commit()
         await self.db.refresh(new_document)
-        return new_document.doc_id
+        return new_document.doc_id, new_document.title, new_document.version, new_document.created_at
