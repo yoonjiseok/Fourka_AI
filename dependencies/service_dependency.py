e8cc7e155cd90d20fec1c6dd896a628e54d29fc7
G9bonwook <gubon0830@gmail.com>
2025-07-24 10:08:43 +0900

    ✨ [feature] feedback 의존성 추가

--- dependencies/service_dependency.py
+++ dependencies/service_dependency.py
@@ -5,13 +5,15 @@ from database.repository.chat_repository import ChatRepository
 from database.repository.document_repository import DocumentRepository
 from database.repository.faq_repository import FAQRepository
 from database.repository.tag_repository import TagRepository
+from database.repository.feedback_repository import FeedbackRepository
 from dependencies.repository_dependency import get_document_repository, get_faq_repository, get_tag_repository, \
-    get_chat_repository
+    get_chat_repository, get_feedback_repository
 from service.chat.chat_service import ChatService
 from service.chunk.chunk_service import ChunkService
 from service.document.document_service import DocumentService
 from service.faq.faq_service import FAQService
 from service.faq.tag_service import TagService
+from service.feedback.feedback_service import FeedbackService
 
 
 def get_document_service(repo: DocumentRepository = Depends(get_document_repository)) -> DocumentService:
@@ -32,3 +34,6 @@ def get_tag_service(tag_repo: TagRepository = Depends(get_tag_repository)) -> Ta
 
 def get_chat_service(repo: ChatRepository = Depends(get_chat_repository)) -> ChatService:
     return ChatService(repo)
+
+def get_feedback_service(repo: FeedbackRepository = Depends(get_feedback_repository)) -> FeedbackService:
+    return FeedbackService(repo)
