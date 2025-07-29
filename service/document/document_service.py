885de86192e15eefcf98f0272a6a1cb4aac8d3bf
Jaemo Lee <68765620+dukesugar@users.noreply.github.com>
2025-07-29 17:33:44 +0900

    document_service.py import 수정함

--- service/document/document_service.py
+++ service/document/document_service.py
@@ -1,4 +1,6 @@
 import os
+import google.generativeai as genai
+from config import settings
 from database.repository.document_repository import DocumentRepository
 from fastapi import UploadFile
 from service.chunk.chunk_service import ChunkService
