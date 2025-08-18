d8d11bd069219e550bdfc76bdb3f701d590bf166
KwonHalim <hr_blizzard@naver.com>
2025-08-18 14:00:00 +0900

    :ambulance: [HotFix] DTO 필요 값 검증 예외 로직 추가

--- exception/exception_handler.py
+++ exception/exception_handler.py
@@ -1,8 +1,8 @@
+from fastapi.exceptions import RequestValidationError
 from starlette.responses import JSONResponse
-from fastapi import Request
+from fastapi import Request, status
 from exception.models.base_exception_model import ErrorResponse, ErrorDetail
 from exception.models.exception import BaseApiException
-
 
 def exception_handler(request: Request, exc: BaseApiException):
     """
@@ -23,3 +23,20 @@ def exception_handler(request: Request, exc: BaseApiException):
         status_code=exc.status_code,
         content=response_model.model_dump()
     )
+
+
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    # 유효성 검사 에러의 세부 정보 가져오기
+    error_detail = exc.errors()[0]
+    field_name = ".".join(map(str, error_detail["loc"]))
+
+    # 커스텀 예외인 BaseApiException 생성
+    custom_exc = BaseApiException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        message="요청에 필수 필드가 누락되었거나 유효하지 않습니다.",
+        reason=f"필드 '{field_name}'에 유효성 오류가 있습니다.",
+        field=field_name
+    )
+
+    # 생성된 커스텀 예외를 기존 exception_handler로 전달
+    return exception_handler(request, custom_exc)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 
 from api.routes.chat.chat import chat_router
 from api.routes.document.document import document_router
@@ -7,7 +8,7 @@ from api.routes.faq.tag import tag_router
 from api.routes.feedback.feedback import feedback_router
 from api.routes.folder.folder import folder_router
 from api.routes.health.health import health_router
-from exception.exception_handler import exception_handler
+from exception.exception_handler import exception_handler, validation_exception_handler
 from exception.models.exception import BaseApiException
 from utils.db import async_engine
 from database.models import Base
@@ -30,6 +31,7 @@ app.include_router(folder_router)
 
 # 채팅 예외 핸들러
 app.exception_handler(BaseApiException)(exception_handler)
+app.exception_handler(RequestValidationError)(validation_exception_handler)
 
 @app.on_event("startup")
 async def init_db() -> None:
